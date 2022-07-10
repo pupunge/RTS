72c7e144d813330cf654645bdb11728bab0a8b0a
pupungeggang@outlook.kr <pupungeggang2@gmail.com>
2022-07-10 21:37:38 +0900

    0.0

--- Script/Scene/SaveSelect.js
+++ Script/Scene/SaveSelect.js
@@ -1,3 +1,8 @@
+var UISaveSelect = {
+     titleText : [8, 48],
+     saveRect : [128, 128, 768, 384],
+};
+
 function loopSaveSelect() {
     displaySaveSelect();
 }
@@ -9,10 +14,21 @@ function displaySaveSelect() {
     context.font = '48px Opensans';
 
     context.clearRect(0, 0, 1024, 640);
+
+    context.fillText(`Select save file.`, UISaveSelect.titleText[0], UISaveSelect.titleText[1]);
+
+    // Save File
+    context.strokeRect(UISaveSelect.saveRect[0], UISaveSelect.saveRect[1], UISaveSelect.saveRect[2], UISaveSelect.saveRect[3]);
 }
 
 function mouseUpSaveSelect(x, y) {
-
+    if (menu === false) {
+        if (state === '') {
+            if (pointInsideRectArray(x, y, UISaveSelect.saveRect)) {
+                scene = 'LevelSelect';
+            }
+        }
+    }
 }
 
 function keyDownSaveSelect(key) {
